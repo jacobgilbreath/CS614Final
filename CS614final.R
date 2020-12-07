50e4a9be7fde18669c1077ed1d1b7a140fbd1449
jacobgilbreath <jgilbrea@chapman.edu>
2020-12-06 23:14:07 -0800

    Initial commit

---
+++ CS614final.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+red=read.csv("Data/winequality-red.csv",sep=";")
+
+
+ggplot(red,aes(x=quality))+geom_bar()
+summary(red)
+summary(lm(quality~fixed.acidity+volatile.acidity+citric.acid,data=red))
+summary(lm(quality~volatile.acidity,data=red))
